0b884062fededc017ad7953dbe340d87af91cf28
Emituofo2012 <liugaojing2012@foxmail.com>
2023-04-17 11:27:28 +0800

    Creat cut tag analysis pipeline script

---
+++ Cut-tag-Bulk.V0.1.py
@@ -0,0 +1,21 @@
+# -*- coding: utf-8 -*-
+"""
+Created on 2022\12\01
+
+@author: liugaojing2012
+
+Email:liugaojing2012@foxmail.com
+
+Any questions email me.
+
+"""
+
+import argparse
+import pysam
+import os
+import re
+import operator
+import numpy
+import multiprocessing
+import glob
+import time
